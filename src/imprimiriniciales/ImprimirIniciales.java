bfbc60215396f4a3946bc69964ea4d85e6654dd0
Kevin MoRo <125381456+Kevin-MR-xd@users.noreply.github.com>
2023-03-09 21:57:31 -0600

    Add files via upload

--- src/imprimiriniciales/ImprimirIniciales.java
+++ src/imprimiriniciales/ImprimirIniciales.java
@@ -3,25 +3,26 @@ import java.util.Scanner;
 
 /**
  *
- * @author maryse
+ * @author El Chistes (Kevin MR)
  *
  * Scanner Este programa imprime las iniciales del nombre introducido por el
  * usuario.
  *
  */
 public class ImprimirIniciales {
-
-    public static void main(String[] args) {
-
-        Scanner stdIn = new Scanner(System.in);
-        String nombre; // nombre
-        String apellido; // apellido
-        
+    Scanner stdIn = new Scanner(System.in);
+     static String nombre = ""; // nombre
+     static String apellido = ""; // apellido
+     
+     public void Inc(String nombre, String apellido){
         System.out.print("Introduzca su nombre y primer apellido separados por un espacio: ");
         nombre = stdIn.next();
         apellido = stdIn.next();
-        System.out.println("Sus iniciales son " + nombre.charAt(0) + apellido.charAt(0) + ".");
-
+        System.out.println("Sus iniciales son " + nombre.charAt(0) +"\r"+ apellido.charAt(0) + ".");
+     }
+    public static void main(String[] args) {
+        ImprimirIniciales inc = new ImprimirIniciales();
+        inc. Inc(nombre,apellido);
     }
 
 }
